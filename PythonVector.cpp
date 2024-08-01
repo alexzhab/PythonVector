124d6b03a40604c48bdbd4e837d89622972702a7
alexzhab <yasashochek@gmail.com>
2024-08-01 08:08:34 +0000

    enh: removed difficult version of copy_array() and used pointers instead

--- PythonVector.cpp
+++ PythonVector.cpp
@@ -18,15 +18,6 @@ namespace {
     for (unsigned i{}; i < size; ++i)
       dst[i] = src[i];
   }
-
-  void copy_array(const double * src, const unsigned src_size, double * dst, const unsigned dst_size, int src_start_id, int src_end_id, int dst_start_id = 0) {
-    msg_assert(src && dst, "Not initialized memory");
-    msg_assert(src_end_id <= src_size, "Out of src array range");
-    msg_assert(dst_start_id + src_end_id - src_start_id <= dst_size, "Out of dst array range");
-
-    for (unsigned i{}; i < src_end_id - src_start_id; ++i)
-      dst[dst_start_id + i] = src[src_start_id + i];
-  }
 }
 
 void PythonVector::init(unsigned size, const double * array) {
@@ -82,7 +73,7 @@ PythonVector PythonVector::range(int n, int m) const {
   const int res_size = m - n;
   PythonVector res(res_size);
   
-  copy_array(m_array, m_size, res.m_array, res_size, n, m);
+  copy_array(m_array + n, res.m_array, res_size);
 
   return res;
 }
@@ -114,8 +105,8 @@ PythonVector PythonVector::operator+(const PythonVector & pv) const {
   const unsigned res_size = m_size + pv.m_size;
   PythonVector res(res_size);
 
-  copy_array(m_array, m_size, res.m_array, res_size, 0, m_size);
-  copy_array(pv.m_array, pv.m_size, res.m_array, res_size, 0, pv.m_size, m_size);
+  copy_array(m_array, res.m_array, m_size);
+  copy_array(pv.m_array, res.m_array + m_size, pv.m_size);
 
   return res;
 }
